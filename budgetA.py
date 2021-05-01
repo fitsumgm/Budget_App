b7bbb62596bd54119d4013d2ebc418f6e8a7d923
fitsum <ifitsumgm@gmail.com>
2021-05-01 12:42:02 -0700

    updated  Budget App using classes and methods

--- budgetA.py
+++ budgetA.py
@@ -2,42 +2,40 @@
 
 class Category:
 
-
     def __init__(self, category, amount): 
 
         self.category = category
         self.amount = amount
 
-   
     def deposit(self, amount):
 
-        amount = input("How much would you like like to deposit in to your {} category: \n".format(self.category)) 
-        return"you've successfully deposited {} to your {} category \n".format(amount, self.category)
+        amount = int(input("How much would you like like to deposit in to your {} category: \n".format(self.category)) )
+        print("you've successfully deposited {} to your {} category \n".format(amount, self.category))
+
+        self.amount += amount;
+        return ("YOur balance is {}".format(self.amount))
+
 
     def withdraw(self, amount):
 
-        amount = input("How much would you like to deposit to your {} category: \n".format(self.category))
-        return "You've successfully withdrawn {} from your {} category".format(amount, self.category)
+        amount = int(input("How much would you like to withdraw from your {} category: \n".format(self.category)))
+        print("You've successfully withdrawn {} from your {} category".format(amount, self.category))
+
+        self.amount -= amount;
+        return self.amount
+
 
     def check_balance(self, amount):
+
         if self.amount > amount:
             return True
         else:
             return False
+            
 
-    def transfer(self, amount, category):
-        amount= input("How much would you like to transfer?")
-
-        #take amount from user
-        #use withdraw funtion to deduct the amount from source category
-        #use deposit function to add amount to desitnation category
-        
-        #self.amount += amount
-
-        #self.amount -= amount
-        #print("You have successfully transfered {} to your {} category").format(amount, self.category)
-
-        pass
+    def transfer(self, amount):
+        self.withdraw(amount) 
+        return Car_category.deposit(amount)
 
 
 Vacation_category = Category('Vacation', 1000)
@@ -45,6 +43,7 @@ Clothing_category = Category('Clothing', 1000)
 Food_category = Category('Food', 1000)
 Car_category = Category('Car Expenses', 1000)
 
-
 print(Vacation_category.deposit(250))
 print(Vacation_category.withdraw(200))
+print(Vacation_category.check_balance(100))
+print(Food_category.transfer(200))
